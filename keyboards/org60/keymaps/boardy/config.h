543cd74e35ff710ff5ab7ddeea3ca1363a22fa0f
deprecatedcoder <RyanSullivan@gmail.com>
2020-08-18 21:27:33 -0400

    Update pinout from custom wiring

---
+++ keyboards/org60/keymaps/boardy/config.h
@@ -0,0 +1,7 @@
+/* Boardy is a bit special and has some wires crossed, so needs these redefined */
+
+#undef MATRIX_COL_PINS
+#define MATRIX_COL_PINS { F0, F1, E6, C7, C6, B6, D4, B1, B7, F5, B4, D7, D6, B3 }
+
+#undef BACKLIGHT_PIN
+#define BACKLIGHT_PIN B5
