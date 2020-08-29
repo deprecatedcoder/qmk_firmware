555d37d25ee00a3759271bfa0bc895e342406c66
deprecatedcoder <RyanSullivan@gmail.com>
2020-08-29 15:13:25 -0400

    Update rules

--- keyboards/org60/keymaps/boardy/rules.mk
+++ keyboards/org60/keymaps/boardy/rules.mk
@@ -1,10 +1,11 @@
 
 # QMK Build Options
-#   change to "no" to disable the options, or define them in the Makefile in 
+#   change to "no" to disable the options, or define them in the Makefile in
 #   the appropriate keymap folder that will get included automatically
 #
-CONSOLE_ENABLE = no         # Enable debugging console
-COMMAND_ENABLE = no         # Commands for debug and configuration
-TAP_DANCE_ENABLE = no 		# Tap Dance skills
-UNICODE_ENABLE = no         # Unicode
-
+BOOTMAGIC_ENABLE = no        # Virtual DIP switch configuration
+CONSOLE_ENABLE = no          # Enable debugging console
+COMMAND_ENABLE = no          # Commands for debug and configuration
+SLEEP_LED_ENABLE = yes       # Makes it so the LEDs don't flash when sleeping...
+TAP_DANCE_ENABLE = no  		 # Tap Dance skills # yes
+UNICODE_ENABLE = yes         # Unicode
