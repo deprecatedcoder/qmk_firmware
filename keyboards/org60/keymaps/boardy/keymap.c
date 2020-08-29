202d704bb0243e0b37bcd331f954cffa93a671c9
deprecatedcoder <RyanSullivan@gmail.com>
2020-08-29 17:46:48 -0400

    Add whitespace and reorganize some keys

--- keyboards/org60/keymaps/boardy/keymap.c
+++ keyboards/org60/keymaps/boardy/keymap.c
@@ -1,17 +1,21 @@
 #include QMK_KEYBOARD_H
 #include "action_layer.h"
 
+
 // Keyboard Layers
 enum keyboard_layers {
   _BASE,        // Base Layer
   _FUNCTION,    // Function Layer
 };
 
+
 enum custom_keycodes {
     SHRUG
 };
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
 
   [_BASE] = LAYOUT(
   /*
@@ -58,9 +62,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├──────────┬────────┴─┬──────┴───┬────┴────────┴────────┴────────┴────────┴────────┴──────┬─┴────────┼──────┬─┴──────┼────────┼────────┤
   //│          │          │          │                                                        │          │ ░░░░ │        │        │        │
   //│          │          │          │                                                        │          │ ░░░░ │        │        │        │
-     KC_LCTL,   KC_LGUI,   KC_LALT,   KC_SPC,                                               MO(_FUNCTION),KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT),
+     KC_LCTL,   KC_LGUI,   KC_LALT,   KC_SPC,                                               MO(_FUNCTION),KC_NO, KC_LEFT, KC_DOWN, KC_RGHT),
   //└──────────┴──────────┴──────────┴────────────────────────────────────────────────────────┴──────────┴──────┴────────┴────────┴────────┘
-
 
   [_FUNCTION] = LAYOUT(
   /*
@@ -72,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ├────────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬─────────────┤
     │            │        │        │        │        │        │        │        │        │        │        │        │        │             │
     │            │        │        │        │ RGB    │ RGB    │ RGB    │ RGB    │        │        │        │        │        │             │
-    │ Reset      │ 7      │ 8      │ 9      │ Toggle │ Mode   │ Pwr+   │ Pwr-   │        │ Insert │ Pause  │ Home   │ End    │ Sleep       │
+    │ Num Lock   │ 7      │ 8      │ 9      │ Toggle │ Mode   │ Pwr+   │ Pwr-   │ Insert │ Calc   │ Pause  │ Home   │ End    │ Sleep       │
     ├────────────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴─────────────┤
     │               │        │        │        │        │        │        │        │        │        │        │        │                   │
     │               │        │        │        │ RGB    │ RGB    │ RGB    │ RGB    │        │        │ Page   │ Page   │                   │
@@ -80,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ├───────────────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴─┬────────┬────────┤
     │                   │        │        │        │ Back   │        │        │ Back   │        │        │        │ ░░░░ │        │ ░░░░░░ │
     │                   │        │        │        │ Light  │ Back   │ Back   │ Light  │ Prev   │ Next   │        │ ░░░░ │        │ ░░░░░░ │
-    │ SHRUG             │ 1      │ 2      │ 3      │ Toggle │ Light+ │ Light- │ Breathe│ Track  │ Track  │        │ ░░░░ │ Up     │ ░░░░░░ │
+    │ SHRUG             │ 1      │ 2      │ 3      │ Toggle │ Light+ │ Light- │ Breathe│ Track  │ Track  │ Reset  │ ░░░░ │ Up     │ ░░░░░░ │
     ├──────────┬────────┴─┬──────┴───┬────┴────────┴────────┴────────┴────────┴────────┴──────┬─┴────────┼──────┬─┴──────┼────────┼────────┤
     │          │          │          │                                                        │          │ ░░░░ │        │        │        │
     │          │          │          │                                                        │          │ ░░░░ │        │        │        │
@@ -91,26 +94,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────┬─────────────────┐
   //│        │        │        │        │        │        │        │        │        │        │        │        │        │                 │
   //│        │        │        │        │        │        │        │        │        │        │        │        │        │                 │
-     KC_GRAVE,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_NO,
+     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_NO,
   //├────────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬─────────────┤
   //│            │        │        │        │        │        │        │        │        │        │        │        │        │             │
   //│            │        │        │        │        │        │        │        │        │        │        │        │        │             │
-     RESET,       KC_7,    KC_8,    KC_9,    RGB_TOG, RGB_MOD, RGB_VAI, RGB_VAD, KC_NO,   KC_INS,  KC_PAUS, KC_HOME, KC_END,  KC_SLEP,
+     KC_NLCK,     KC_P7,   KC_P8,   KC_P9,   RGB_TOG, RGB_MOD, RGB_VAI, RGB_VAD, KC_INS,  KC_CALC, KC_PAUS, KC_HOME, KC_END,  KC_SLEP,
   //├────────────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴─────────────┤
   //│               │        │        │        │        │        │        │        │        │        │        │        │                   │
   //│               │        │        │        │        │        │        │        │        │        │        │        │                   │
-     KC_CAPS,        KC_4,    KC_5,    KC_6,    RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, KC_NO,   KC_NO,   KC_PGUP, KC_PGDN, KC_NO, KC_RSFT,
+     KC_CLCK,        KC_P4,   KC_P5,   KC_P6,   RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, KC_NO,   KC_NO,   KC_PGUP, KC_PGDN, KC_NO, KC_RSFT,
   //├───────────────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴─┬────────┬────────┤
   //│                   │        │        │        │        │        │        │        │        │        │        │ ░░░░ │        │ ░░░░░░ │
   //│                   │        │        │        │        │        │        │        │        │        │        │ ░░░░ │        │ ░░░░░░ │
-     SHRUG,   KC_NO,     KC_1,    KC_2,    KC_3,    BL_TOGG, BL_INC,  BL_DEC,  BL_BRTG,   KC_MPRV, KC_MNXT, KC_NO,   KC_NO, KC_VOLU, KC_NO,
+     SHRUG,   KC_NO,     KC_P1,   KC_P2,   KC_P3,   BL_TOGG, BL_INC,  BL_DEC,  BL_BRTG, KC_MPRV, KC_MNXT, RESET,   KC_NO, KC_VOLU, KC_NO,
   //├──────────┬────────┴─┬──────┴───┬────┴────────┴────────┴────────┴────────┴────────┴──────┬─┴────────┼──────┬─┴──────┼────────┼────────┤
   //│          │          │          │                                                        │          │ ░░░░ │        │        │        │
   //│          │          │          │                                                        │          │ ░░░░ │        │        │        │
-     KC_LCTL,   KC_LGUI,   KC_LALT,   KC_0,                                                 MO(_FUNCTION),KC_NO, KC_MUTE, KC_VOLD, KC_MPLY),
+     KC_LCTL,   KC_LGUI,   KC_LALT,   KC_P0,                                                MO(_FUNCTION),KC_NO, KC_MUTE, KC_VOLD, KC_MPLY),
   //└──────────┴──────────┴──────────┴────────────────────────────────────────────────────────┴──────────┴──────┴────────┴────────┴────────┘
 
-};
+  };
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
@@ -125,6 +129,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
+
 
 // Loop
 void matrix_scan_user(void) {
