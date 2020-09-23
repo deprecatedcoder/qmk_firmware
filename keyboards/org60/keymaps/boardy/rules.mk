df3a1807305a0e90cc95b3596aa37ccfc87b6c76
deprecatedcoder <RyanSullivan@gmail.com>
2020-09-23 16:47:29 -0400

    Remove unused MOUSEKEY_ENABLE to reduce overall firmware size

--- keyboards/org60/keymaps/boardy/rules.mk
+++ keyboards/org60/keymaps/boardy/rules.mk
@@ -6,6 +6,7 @@
 BOOTMAGIC_ENABLE = no        # Virtual DIP switch configuration
 CONSOLE_ENABLE = no          # Enable debugging console
 COMMAND_ENABLE = no          # Commands for debug and configuration
+MOUSEKEY_ENABLE = no         # Mouse keys
 SLEEP_LED_ENABLE = yes       # Makes it so the LEDs don't flash when sleeping...
 TAP_DANCE_ENABLE = yes		 # Tap Dance skills
 UNICODEMAP_ENABLE = yes      # Even more Unicode
